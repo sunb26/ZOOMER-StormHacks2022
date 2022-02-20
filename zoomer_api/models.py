1e32f107a9aef083da2f6094b06e87f496ce3247
Ben Sun <sunb26@mcmaster.ca>
2022-02-20 05:36:42 -0500

    Delete zoomer_api directory

--- zoomer_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-# Create your views here.
-class RegisterUser(models.Model):
-    # Creates user
-    user = models.CharField(max_length=100, blank=False, default="")
